4060963ceeebc2807549c2717755b7e78f752211
grush <grush@DESKTOP-P5A2MM5>
2024-05-06 12:25:39 +0300

    Добавьте файлы проекта.

---
+++ Lab10/Lab10.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct ApartmentOwner {
+    string lastName;
+    int apartmentNumber;
+    int numberOfResidents;
+};
+
+int main() {
+    vector<ApartmentOwner> apartmentOwners;
+
+
+    int n;
+    cout << "Enter the number of apartment owners: ";
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        ApartmentOwner owner;
+        cout << "Last name of the owner: ";
+        cin >> owner.lastName;
+        cout << "Apartment number: ";
+        cin >> owner.apartmentNumber;
+        cout << "Number of residents: ";
+        cin >> owner.numberOfResidents;
+        apartmentOwners.push_back(owner);
+    }
+
+
+    cout << "\nFull list of apartment owners:\n";
+    for (const auto& owner : apartmentOwners) {
+        cout << "Last name: " << owner.lastName << ", Apartment number: " << owner.apartmentNumber
+            << ", Number of residents: " << owner.numberOfResidents << endl;
+    }
+
+
+    cout << "\nApartment numbers with more than 4 residents:\n";
+    for (const auto& owner : apartmentOwners) {
+        if (owner.numberOfResidents > 4) {
+            cout << "Apartment number: " << owner.apartmentNumber << ", Number of residents: " << owner.numberOfResidents << endl;
+        }
+    }
+
+    return 0;
+}
